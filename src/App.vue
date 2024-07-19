a0bef40e7a05073f7ce5efe861332f3ed571f6e6
cycleccc <2991205548@qq.com>
2024-07-19 22:39:30 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <p>Vue3 wangEditor demo</p>
+    <BasicEditor></BasicEditor>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import BasicEditor from './components/BasicEditor.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    BasicEditor,
+  },
+};
+</script>
+
+<style></style>
